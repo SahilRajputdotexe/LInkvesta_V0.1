1da2d5808d2b29074fcbf45ce10be913bb84c039
Divyanshu523 <divyanshubhardwaj23@gmail.com>
2025-01-09 20:32:30 +0530

    about page created
    made changes to global css, about page and adjustment of laptop component

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,8 +1,33 @@
+
+import { LaptopMockup } from "@/components/LaptopMockup";
+
+
 export default function About() {
     return (
       <div className="p-6">
-        <h1 className="text-3xl font-bold">About Us</h1>
-        <p className="mt-4">We help brands grow by providing expert consulting and strategies.</p>
+      <div className="text-white flex flex-col items-center pt-0 w-full">
+  <h1 className="text-4xl sm:text-4xl md:text-4xl lg:text-7xl font-bold mb-4 text-center animate-gradient-text">
+    <span className="text-transparent bg-clip-text bg-gradient-to-t from-gray-400 to-gray-100">About </span> 
+    <span className="text-transparent bg-clip-text bg-gradient-to-b from-[#35747D] via-cyan-600 to-gray-400">
+      Us
+    </span>
+  </h1>
+  </div>
+
+        <div className="mt-8 flex justify-center">
+        <div className="relative w-[800px] h-[400px] " >
+          <div className="absolute top-[10%] left-[10%] w-[80%] h-[70%] sm:top-[10px] sm:left-[-140px] sm:w-[598px] sm:h-[285px] ">
+            <LaptopMockup />
+          </div>
+        </div>
+
+        <div className="absolute top-[50%] right-[10%] text-white-600 text-lg font-bold  " >
+        <p>We believe in delivering excellence through cutting-edge technology and</p>
+        <p> user-centric design. Explore our range of products and services</p>
+        <p> tailored to meet your unique needs. Let’s shape the future, together.</p>
+
+          </div>
+      </div>
       </div>
     );
   }
--- components/LaptopMockup.tsx
+++ components/LaptopMockup.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export const LaptopMockup=async()=> {
+export const LaptopMockup= async() => {
     return (
         <div className="mockup-laptop relative">
        
@@ -13,7 +13,6 @@ export const LaptopMockup=async()=> {
         />
   
         <div className=" artboard rounded-xl bg-black border absolute top-[8%] left-[12%] w-[76%] h-[67%] sm:top-[42px] sm:left-[80px] sm:w-[445px] sm:h-[285px]">
-      
           <div className="bg-black rounded-xl flex justify-center px-4 py-16">
           </div>
         </div>
